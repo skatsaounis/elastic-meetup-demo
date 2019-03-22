b57364c862f2387ca5b010b443230d5d96309d76
Stamatis Katsaounis <katsaouniss@gmail.com>
2019-03-22 10:45:56 -0400

    Add script for building Filebeat with NATS module
    Signed-off-by: Stamatis Katsaounis <katsaouniss@gmail.com>

---
+++ build_filebeat.sh
@@ -0,0 +1,16 @@
+if hash go 2>/dev/null; then
+  wd=$(pwd)
+  GOPATH="${GOPATH:-$HOME/go}"
+
+  mkdir -p ${GOPATH}/src/github.com/elastic
+  git config --global http.sslVerify false
+  git clone https://github.com/elastic/beats.git ${GOPATH}/src/github.com/elastic/beats
+  cd ${GOPATH}/src/github.com/elastic/beats/filebeat
+  make filebeat
+  python ../libbeat/scripts/unpack_dashboards.py --glob="./module/nats/_meta/kibana/7/dashboard/Filebeat-nats-overview.json"
+  cp -pr ${GOPATH}/src/github.com/elastic/beats/filebeat ${wd}/build/filebeat
+
+  rm -rf ${GOPATH}/src/github.com/elastic
+else
+  echo "Go is not installed in the system. Aborting."
+fi
