3a640f70b83a198f0d194fdb717aa18c053adcd2
Stamatis Katsaounis <katsaouniss@gmail.com>
2019-04-06 14:49:59 +0300

    Use version 7 for all images
    Signed-off-by: Stamatis Katsaounis <katsaouniss@gmail.com>

--- build_filebeat.sh
+++ build_filebeat.sh
@@ -6,6 +6,7 @@ if hash go 2>/dev/null; then
   git config --global http.sslVerify false
   git clone https://github.com/elastic/beats.git ${GOPATH}/src/github.com/elastic/beats
   cd ${GOPATH}/src/github.com/elastic/beats/filebeat
+  git checkout 7.x
   make filebeat
   python ../libbeat/scripts/unpack_dashboards.py --glob="./module/nats/_meta/kibana/7/dashboard/Filebeat-nats-overview.json"
   cp -pr ${GOPATH}/src/github.com/elastic/beats/filebeat ${wd}/build/filebeat
